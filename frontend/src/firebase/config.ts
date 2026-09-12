99029018e123d87bad3bcf9a4cb1f69746c02718
anayarora <s4095243@student.rmit.edu.au>
2026-09-13 00:39:31 +1000

    chore: update firebase config

--- frontend/src/firebase/config.ts
+++ frontend/src/firebase/config.ts
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAnVgkoUb5EkUHAqXUzXIaQHwTPma3f7PI",
+  authDomain: "fsc-crm-team-a.firebaseapp.com",
+  projectId: "fsc-crm-team-a",
+  storageBucket: "fsc-crm-team-a.firebasestorage.app",
+  messagingSenderId: "1074666109912",
+  appId: "1:1074666109912:web:ec8fd7ae55078b158750ad",
+  measurementId: "G-22HK1WM5QZ"
 };
 
 export const app = initializeApp(firebaseConfig);
